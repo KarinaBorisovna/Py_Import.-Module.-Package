d3d9a9838c70b0bf96bcfd2a3e1936ecb0f1acdd
Karina Vinogradova <x-mower@mail.ru>
2021-12-05 18:55:34 +0300

    datetime changed

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import package_application.salary as salary
 import package_application.package_db.people as people
+from datetime import datetime
 
 
 if __name__ == '__main__':
     # name = input('Введите имя сотрудника: ')
     name = 'Peter'
+
+    print(datetime.now())
+
     my_post = people.get_employees(name)
     print(f'Должность сотрудника {name} - {my_post}')
 
--- package_application/salary.py
+++ package_application/salary.py
@@ -1,7 +1,4 @@
-from datetime import datetime
-
 def calculate_salary():
-    print(datetime.now())
     sum = 250000
 
     return sum
